266dfdccd383553f589545f8135069cd3eb39420
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-12-06 10:39:13 -0800

    added packages

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+
+exports.spotify = {
+    id: process.env.fb04f6c665ed49db94f9293798f6e4ed,
+    secret: process.env.17f0f62b38cd4daf88214820ffd6feaf
+  };
+  
---
+++ liri.js
@@ -0,0 +1,13 @@
+require('dotenv').config(); 
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+const axios= require('axios');
+
+var Spotify = require('node-spotify-api');
+
+
+
+console.log('hellllllloooo')
