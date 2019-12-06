d6c0ad5b62a7e92f3ba5187a7e31aff6ffeec134
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-12-06 10:46:46 -0800

    added to random.txt and keys.js

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 
+console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.fb04f6c665ed49db94f9293798f6e4ed,
-    secret: process.env.17f0f62b38cd4daf88214820ffd6feaf
-  };
-  
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
