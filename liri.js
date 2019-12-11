27dd855fa20394474afda58ca514018c39cb8228
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-12-11 10:28:18 -0800

    testing variables

--- liri.js
+++ liri.js
@@ -13,11 +13,12 @@ const axios = require("axios");
 // "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 var userCommand = process.argv[2];
-var searchTerm = process.argv.slice(3).join("+");
+
 
 console.log(userCommand, searchTerm);
 
 if (userCommand === "concert-this") {
+    var searchTerm = process.argv.slice(3).join("+");
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
@@ -26,13 +27,33 @@ if (userCommand === "concert-this") {
     )
     .then(function(response) {
       for (let i = 0; i < response.data.length; i++) {
-            console.log("Concert Location Date and Time for"+" "+ process.argv.slice(3).join(" ")+":")
-            console.log(response.data[i].venue.name);
-            console.log(response.data[i].venue.city+", "+response.data[i].venue.region);
-            console.log(response.data[i].datetime)
-            console.log('------------------')
+        console.log(
+          "Concert Location Date and Time for" +
+            " " +
+            process.argv.slice(3).join(" ") +
+            ":"
+        );
+        console.log(response.data[i].venue.name);
+        console.log(
+          response.data[i].venue.city + ", " + response.data[i].venue.region
+        );
+        console.log(response.data[i].datetime); //fix later
+        console.log("------------------");
       }
-
-      
     });
 }
+
+
+
+
+if (userCommand === "spotify-this-song") {
+    var songTitle=process.argv.slice(3).join(" ")
+    spotify.search({ type: 'track', query:songTitle  }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(songTitle)
+      console.log(data); 
+      });
+}
+
